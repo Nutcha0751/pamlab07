4286c2556bda3857a74ef090ef5a3ce6b738c177
Nutcha0751 <nutcha_kham@cmu.ac.th>
2023-12-08 14:24:17 +0700

    lab7_1.cpp completed

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-
-int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+using namespace std; 
+int main(){
+    double a  =  20, b = 30;
+    double c = 4;
+    double outpot;
 
     outpot = a - b/c;
-    count << output: << output << \n;
+    cout << "output: " << outpot << "\n";
     return 0;
 }
