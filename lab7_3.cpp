7107a9760c65bc554eee39d0941494c5621d7b1f
Nutcha0751 <nutcha_kham@cmu.ac.th>
2023-12-08 16:12:59 +0700

    lab7_3.cpp completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if(x < 'A' || x > 'Z'){
+		return '0';
+	}
+	else{
+		if(x == 'A'){
+			return 'Z';
+		}
+		else{
+			return x-1;
+		}
+	}
+	return x;
 }
 
 int main(){
